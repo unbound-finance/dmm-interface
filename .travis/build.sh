87f4d4fad797677f5dcb189b4444b501bd31bb83
Thang Dinh <vietthang207@gmail.com>
2021-10-15 13:35:22 +0700

    test

--- .travis/build.sh
+++ .travis/build.sh
@@ -4,16 +4,11 @@ set -euo pipefail
 
 printf 'Building swap from commit %s\n' "$TRAVIS_COMMIT"
 if [[ "$TRAVIS_BRANCH" == "ci-cd" ]]; then
+	rm -r build
   echo "Start running build-ropsten"
   yarn build-ropsten
   echo "Finished running build-ropsten"
   ls build
-  mv build build-ropsten
-  ls build-ropsten
-  echo "Start build prod"
-  yarn build-production
-  ls build
-  echo "Finish build prod"
 elif [[ "$TRAVIS_BRANCH" == "staging" ]]; then
   echo "Start running build-staging"
   yarn build-staging
